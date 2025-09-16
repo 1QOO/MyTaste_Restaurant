807b81b4add070504d3c1584410b3f19aea6459d
1Q00 <iqro.s.ahfadz@gmail.com>
2025-09-16 21:05:05 +0700

    Finished form layout.

--- src/comps/form.jsx
+++ src/comps/form.jsx
@@ -1,18 +1,20 @@
 const ReservForm = ()=>{
     
     return (
-        <fieldset>
-            <legend>Reserfation Form:</legend>
-            <form>
-                <label for="name">Name :</label>
-                <input type="text" name="name" id="name" />
-                <label for="email">Email :</label>
-                <input type="email" name="email" id="email" />
-                <label for="phone">Phone number :</label>
-                <input type="phone" name="phone" id="phone" />
-                <submit>Submit</submit>
-            </form>
-        </fieldset>
+        <section className="bg-amber-900 p-[1rem]">
+            <fieldset className="text-white border-solid border-[0.2rem] rounded-[1rem]">
+                <legend>Reserfation Form:</legend>
+                <form className="p-[1.5rem]">
+                    <label htmlFor="name">Name :</label><br />
+                    <input type="text" name="name" id="name" className="bg-white w-full" /><br /><br />
+                    <label htmlFor="email">Email :</label><br />
+                    <input type="email" name="email" id="email" className="bg-white w-full" /><br /><br />
+                    <label htmlFor="phone">Phone number :</label><br />
+                    <input type="phone" name="phone" id="phone" className="bg-white w-full" /><br /><br />
+                    <button type="submit" className="bg-amber-300 text-amber-900 font-bold p-[0.3rem] rounded-[0.3rem] mx-auto block">Submit</button><br />
+                </form>
+            </fieldset>
+        </section>
     )
 }
 
