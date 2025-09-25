2fa689953ecc8271a0178feddaa4a1bdf5776131
1QOO <iqro.s.ahfadz@gmail.com>
2025-09-25 16:48:43 +0700

    Finished responsiveness of gallery images.

--- src/comps/gallery.jsx
+++ src/comps/gallery.jsx
@@ -4,11 +4,11 @@ import '../utils/gallery.css';
 
 //IMAGE POSITION FOR INFINITE CAROUSEL
 const carouselState = [
-    {transform: "translateX(calc(40vw * -2)) scale(0.8)", duration : "0.5s", center: false, pos: "corner-left"},
-    {transform: "translateX(calc(40vw * -1)) scale(0.8)", duration : "0.5s", center: false},
-    {transform: "translateX(calc(40vw * 0)) scale(1)", duration : "0.5s", center: true},
-    {transform: "translateX(calc(40vw * 1)) scale(0.8)", duration : "0.5s", center: false},
-    {transform: "translateX(calc(40vw * 2)) scale(0.8)", duration : "0.5s", center: false, pos: "corner-right"},
+    {transform: "translateX(calc(100% * -2)) scale(0.6)", duration : "0.5s", center: false, pos: "corner-left"},
+    {transform: "translateX(calc(100% * -1)) scale(0.6)", duration : "0.5s", center: false},
+    {transform: "translateX(calc(100% * 0)) scale(1)", duration : "0.5s", center: true},
+    {transform: "translateX(calc(100% * 1)) scale(0.6)", duration : "0.5s", center: false},
+    {transform: "translateX(calc(100% * 2)) scale(0.6)", duration : "0.5s", center: false, pos: "corner-right"},
 ]
 
 const Gallery = ()=>{
@@ -41,13 +41,22 @@ const Gallery = ()=>{
     }
 
     return (
-        <div className="text-center bg-amber-900 min-h-[60vw] text-amber-300 relative h-[100px]">
+        <div className="text-center bg-amber-900 text-amber-300 relative">
             <h2>Table for reservation</h2>
-            <div className="flex justify-center items-center h-[60vw] overflow-clip mask-x-from-70% mask-x-to-90%">
+            <div className="flex justify-center items-center overflow-clip m-auto mask-x-from-70% mask-x-to-90% md:lg:w-[50%]">
                 <Images active={active} images={images} />
             </div>
-            <button className="text-amber-900 font-bold bg-amber-300 px-[0.5rem] absolute left-[0.5rem] top-[50%]" onClick={handleSlideLeft}>&lt;</button>
-            <button className="text-amber-900 font-bold bg-amber-300 px-[0.5rem] absolute right-[0.5rem] top-[50%]" onClick={handleSlideRight}>&gt;</button>
+            <button className="
+                text-amber-900 font-bold bg-amber-300 px-[0.5rem] absolute left-[0.5rem] md:lg:left-[20%] top-[50%] 
+                md:lg:right-[20%] md:lg:rounded-full md:lg:size-[40px] md:lg:text-2xl" 
+                onClick={handleSlideLeft}>&lt;
+            </button>
+            <button className="
+                text-amber-900 font-bold bg-amber-300 px-[0.5rem] absolute right-[0.5rem] top-[50%] 
+                md:lg:right-[20%] md:lg:rounded-full md:lg:size-[40px] md:lg:text-2xl
+                "
+                onClick={handleSlideRight}>&gt;
+            </button>
             <div className="m-auto py-1">
             </div>
         </div>
@@ -55,20 +64,21 @@ const Gallery = ()=>{
 }
 
 const Images = ({active, images})=>{
-    const style = "image-card shrink-0 text-center w-[40vw] absolute";
+    const style = "image-card shrink-0 text-center size-fit realtive";
 
     return (
-        <div className="carousel-wrapper w-[40vw] h-[40vw] relative">
+        <div className="carousel-wrapper size-fit relative">
 
             {
             images.map((item,i)=>{
+
                 return (
                     <div key={i+1} id={`gallery-${i}img`} className={style} style={{
                         "--transform": active[i].transform,
-                        "--width": active[i].center?"40vw":"30vw",
                         "--duration": active[i].duration,
+                        "--position": active[i].center?"relative":"absolute",
                     }} >
-                        <img src={item.img} alt={item.alt} className="size-full" />
+                        <img src={item.img} alt={item.alt} className="size-[120px] md:size-[240px]" />
                         <span>{item.name}</span>
                     </div>
                 )
